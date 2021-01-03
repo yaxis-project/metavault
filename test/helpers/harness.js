ddb4597430da6e448a86d1b64a77bd237a70f172
transferAndCall <transferAndCall@protonmail.com>
2021-01-03 12:57:13 -0600

    Refactor strategy tests to use a common harness

---
+++ test/helpers/harness.js
@@ -0,0 +1,181 @@
+const { constants, ether } = require('@openzeppelin/test-helpers');
+const { INIT_BALANCE, MAX, fromWei, fromWeiWithDecimals } = require('./common');
+const MockERC20 = artifacts.require('MockERC20');
+const MockStableSwap3Pool = artifacts.require('MockStableSwap3Pool');
+const MockUniswapRouter = artifacts.require('MockUniswapRouter');
+const StableSwap3PoolConverter = artifacts.require('StableSwap3PoolConverter');
+const StrategyControllerV2 = artifacts.require('StrategyControllerV2');
+const yAxisMetaVault = artifacts.require('yAxisMetaVault');
+const yAxisMetaVaultManager = artifacts.require('yAxisMetaVaultManager');
+const yAxisMetaVaultHarvester = artifacts.require('yAxisMetaVaultHarvester');
+
+exports.afterStrategySetup = async () => {
+    await setupProtocol();
+    await fundContracts();
+    await setupUser();
+};
+
+exports.beforeStrategySetup = async (accounts) => {
+    await setupAccounts(accounts);
+    await deployTokenContracts();
+    await deployProtocolContracts();
+};
+
+const deployProtocolContracts = async () => {
+    globalThis.vaultManager = await yAxisMetaVaultManager.new(globalThis.yax.address);
+    globalThis.vault = await yAxisMetaVault.new(
+        globalThis.dai.address,
+        globalThis.usdc.address,
+        globalThis.usdt.address,
+        globalThis.t3crv.address,
+        globalThis.yax.address,
+        ether('1'),
+        1
+    );
+    globalThis.controller = await StrategyControllerV2.new(globalThis.vaultManager.address);
+    globalThis.harvester = await yAxisMetaVaultHarvester.new(
+        globalThis.vaultManager.address,
+        globalThis.controller.address
+    );
+    globalThis.stableSwap3Pool = await MockStableSwap3Pool.new(
+        globalThis.dai.address,
+        globalThis.usdc.address,
+        globalThis.usdt.address,
+        globalThis.t3crv.address
+    );
+    globalThis.converter = await StableSwap3PoolConverter.new(
+        globalThis.dai.address,
+        globalThis.usdc.address,
+        globalThis.usdt.address,
+        globalThis.t3crv.address,
+        globalThis.stableSwap3Pool.address,
+        globalThis.vaultManager.address
+    );
+    globalThis.unirouter = await MockUniswapRouter.new(constants.ZERO_ADDRESS);
+};
+
+exports.deployProtocolContracts = deployProtocolContracts;
+
+const deployTokenContracts = async () => {
+    globalThis.yax = await MockERC20.new('yAxis', 'YAX', 18);
+    globalThis.dai = await MockERC20.new('Dai Stablecoin', 'DAI', 18);
+    globalThis.usdc = await MockERC20.new('USD Coin', 'USDC', 6);
+    globalThis.usdt = await MockERC20.new('Tether', 'USDT', 6);
+    globalThis.weth = await MockERC20.new('Wrapped ETH', 'WETH', 18);
+    globalThis.t3crv = await MockERC20.new('Curve.fi DAI/USDC/USDT', '3Crv', 18);
+};
+
+exports.deployTokenContracts = deployTokenContracts;
+
+const fundContracts = async () => {
+    await globalThis.yax.mint(globalThis.vault.address, INIT_BALANCE);
+    await globalThis.dai.mint(globalThis.stableSwap3Pool.address, INIT_BALANCE);
+    await globalThis.usdc.mint(globalThis.stableSwap3Pool.address, '1000000000');
+    await globalThis.usdt.mint(globalThis.stableSwap3Pool.address, '1000000000');
+    await globalThis.t3crv.mint(globalThis.stableSwap3Pool.address, INIT_BALANCE);
+    await globalThis.yax.mint(globalThis.unirouter.address, INIT_BALANCE);
+    await globalThis.weth.mint(globalThis.unirouter.address, INIT_BALANCE);
+    await globalThis.dai.mint(globalThis.unirouter.address, INIT_BALANCE);
+    await globalThis.usdc.mint(globalThis.unirouter.address, INIT_BALANCE);
+    await globalThis.usdt.mint(globalThis.unirouter.address, INIT_BALANCE);
+};
+
+exports.fundContracts = fundContracts;
+
+exports.printBalances = async (title) => {
+    console.log(title);
+    console.log(
+        'vault 3CRV:     ',
+        fromWei(await globalThis.t3crv.balanceOf(globalThis.vault.address))
+    );
+    console.log(
+        'vault MVLT:     ',
+        fromWei(await globalThis.vault.balanceOf(globalThis.vault.address))
+    );
+    console.log('vault Supply:   ', fromWei(await globalThis.vault.totalSupply()));
+    console.log('-------------------');
+    console.log(
+        'bob balances:    %s DAI/ %s USDC/ %s USDT/ %s 3CRV/ %s YAX',
+        fromWei(await globalThis.dai.balanceOf(globalThis.user)),
+        fromWeiWithDecimals(await globalThis.usdc.balanceOf(globalThis.user), 6),
+        fromWeiWithDecimals(await globalThis.usdt.balanceOf(globalThis.user), 6),
+        fromWei(await globalThis.t3crv.balanceOf(globalThis.user)),
+        fromWei(await globalThis.yax.balanceOf(globalThis.user))
+    );
+    console.log(
+        'bob staked:     ',
+        fromWei((await globalThis.vault.userInfo(globalThis.user)).amount)
+    );
+    console.log('-------------------');
+    console.log(
+        'stakingPool YAX:',
+        fromWei(await globalThis.yax.balanceOf(globalThis.stakingPool))
+    );
+    console.log(
+        'treasury YAX:   ',
+        fromWei(await globalThis.yax.balanceOf(globalThis.treasury))
+    );
+    console.log('-------------------');
+};
+
+const setupAccounts = (accounts) => {
+    globalThis.deployer = accounts[0];
+    globalThis.treasury = accounts[1];
+    globalThis.stakingPool = accounts[2];
+    globalThis.user = accounts[3];
+};
+
+exports.setupAccounts = setupAccounts;
+
+const setupProtocol = async () => {
+    await globalThis.vault.setConverter(globalThis.converter.address);
+    await globalThis.vault.setVaultManager(globalThis.vaultManager.address);
+    await globalThis.vault.setTreasuryWallet(globalThis.treasury);
+    await globalThis.vault.setController(globalThis.controller.address);
+    await globalThis.vaultManager.setVaultStatus(globalThis.vault.address, true);
+    await globalThis.vaultManager.setControllerStatus(globalThis.controller.address, true);
+    await globalThis.vaultManager.setTreasury(globalThis.treasury);
+    await globalThis.vaultManager.setStakingPool(globalThis.stakingPool);
+    await globalThis.vaultManager.setHarvester(globalThis.harvester.address);
+    await globalThis.harvester.setController(globalThis.controller.address);
+    await globalThis.harvester.addStrategy(
+        globalThis.t3crv.address,
+        globalThis.strategy.address,
+        0
+    );
+    await globalThis.harvester.setHarvester(globalThis.deployer, true);
+    await globalThis.controller.setVault(globalThis.t3crv.address, globalThis.vault.address);
+    await globalThis.controller.setConverter(
+        globalThis.t3crv.address,
+        globalThis.dai.address,
+        globalThis.converter.address
+    );
+    await globalThis.controller.addStrategy(
+        globalThis.t3crv.address,
+        globalThis.strategy.address,
+        0
+    );
+    await globalThis.converter.setStrategy(globalThis.strategy.address, true);
+    await globalThis.strategy.setUnirouter(globalThis.unirouter.address);
+    await globalThis.strategy.approveForSpender(
+        globalThis.weth.address,
+        globalThis.unirouter.address,
+        MAX
+    );
+};
+
+exports.setupProtocol = setupProtocol;
+
+const setupUser = async () => {
+    await globalThis.dai.mint(globalThis.user, INIT_BALANCE);
+    await globalThis.usdc.mint(globalThis.user, '1000000000');
+    await globalThis.usdt.mint(globalThis.user, '1000000000');
+    await globalThis.t3crv.mint(globalThis.user, INIT_BALANCE);
+    await globalThis.dai.approve(globalThis.vault.address, MAX, { from: globalThis.user });
+    await globalThis.usdc.approve(globalThis.vault.address, MAX, { from: globalThis.user });
+    await globalThis.usdt.approve(globalThis.vault.address, MAX, { from: globalThis.user });
+    await globalThis.t3crv.approve(globalThis.vault.address, MAX, { from: globalThis.user });
+    await globalThis.vault.approve(globalThis.vault.address, MAX, { from: globalThis.user });
+};
+
+exports.setupUser = setupUser;
--- test/metavault/StrategyDforce.test.js
+++ test/metavault/StrategyDforce.test.js
@@ -1,255 +1,154 @@
-const { constants, ether } = require('@openzeppelin/test-helpers');
+const { INIT_BALANCE, MAX, ether, verbose } = require('../helpers/common');
+const {
+    afterStrategySetup,
+    beforeStrategySetup,
+    printBalances
+} = require('../helpers/harness');
 
-const yAxisMetaVault = artifacts.require('yAxisMetaVault');
-const yAxisMetaVaultManager = artifacts.require('yAxisMetaVaultManager');
-const yAxisMetaVaultHarvester = artifacts.require('yAxisMetaVaultHarvester');
-
-const StableSwap3PoolConverter = artifacts.require('StableSwap3PoolConverter');
-
-const StrategyControllerV2 = artifacts.require('StrategyControllerV2');
 const StrategyDforce = artifacts.require('StrategyDforce');
-
 const MockERC20 = artifacts.require('MockERC20');
-const MockStableSwap3Pool = artifacts.require('MockStableSwap3Pool');
-const MockUniswapRouter = artifacts.require('MockUniswapRouter');
 const MockDErc20 = artifacts.require('MockDErc20');
 const MockDRewards = artifacts.require('MockDRewards');
 
-const verbose = process.env.VERBOSE;
-
-const { fromWei } = web3.utils;
-const MAX = web3.utils.toTwosComplement(-1);
-const INIT_BALANCE = ether('1000');
-
-function fromWeiWithDecimals(num, decimals = 18) {
-    num = Number.parseFloat(String(num));
-    for (let i = 0; i < decimals; i++) num = num * 0.1;
-    return num.toFixed(2);
-}
-
-contract('StrategyDforce', async ([deployer, treasury, stakingPool, bob]) => {
+contract('StrategyDforce', async (accounts) => {
     before(async () => {
-        // Token deployments
-        this.yax = await MockERC20.new('yAxis', 'YAX', 18);
-        this.dai = await MockERC20.new('Dai Stablecoin', 'DAI', 18);
-        this.usdc = await MockERC20.new('USD Coin', 'USDC', 6);
-        this.usdt = await MockERC20.new('Tether', 'USDT', 6);
-        this.weth = await MockERC20.new('Wrapped ETH', 'WETH', 18);
-        this.t3crv = await MockERC20.new('Curve.fi DAI/USDC/USDT', '3Crv', 18);
+        await beforeStrategySetup(accounts);
 
-        this.df = await MockERC20.new('dForce', 'DF', 18);
-        this.dDai = await MockDErc20.new('dForce DAI', 'dDAI', this.dai.address);
-        this.dRewards = await MockDRewards.new(this.dDai.address, this.df.address, 100);
-
-        // Protocol contract deployments
-        this.vaultManager = await yAxisMetaVaultManager.new(this.yax.address);
-        this.vault = await yAxisMetaVault.new(
-            this.dai.address,
-            this.usdc.address,
-            this.usdt.address,
-            this.t3crv.address,
-            this.yax.address,
-            ether('1'),
-            1
+        globalThis.df = await MockERC20.new('dForce', 'DF', 18);
+        globalThis.dDai = await MockDErc20.new('dForce DAI', 'dDAI', globalThis.dai.address);
+        globalThis.dRewards = await MockDRewards.new(
+            globalThis.dDai.address,
+            globalThis.df.address,
+            100
         );
-        this.controller = await StrategyControllerV2.new(this.vaultManager.address);
-        this.harvester = await yAxisMetaVaultHarvester.new(
-            this.vaultManager.address,
-            this.controller.address
+        globalThis.strategy = await StrategyDforce.new(
+            globalThis.dai.address,
+            globalThis.dDai.address,
+            globalThis.dRewards.address,
+            globalThis.df.address,
+            globalThis.converter.address,
+            globalThis.controller.address,
+            globalThis.vaultManager.address,
+            globalThis.weth.address
         );
-        this.stableSwap3Pool = await MockStableSwap3Pool.new(
-            this.dai.address,
-            this.usdc.address,
-            this.usdt.address,
-            this.t3crv.address
+        await globalThis.strategy.approveForSpender(
+            globalThis.df.address,
+            globalThis.unirouter.address,
+            MAX
         );
-        this.converter = await StableSwap3PoolConverter.new(
-            this.dai.address,
-            this.usdc.address,
-            this.usdt.address,
-            this.t3crv.address,
-            this.stableSwap3Pool.address,
-            this.vaultManager.address
-        );
-        this.unirouter = await MockUniswapRouter.new(constants.ZERO_ADDRESS);
+        await globalThis.df.mint(globalThis.unirouter.address, INIT_BALANCE);
+        await globalThis.df.mint(globalThis.dRewards.address, INIT_BALANCE);
 
-        // Strategy contract deployment
-        this.strategy = await StrategyDforce.new(
-            this.dai.address,
-            this.dDai.address,
-            this.dRewards.address,
-            this.df.address,
-            this.converter.address,
-            this.controller.address,
-            this.vaultManager.address,
-            this.weth.address
-        );
-
-        // Protocol setup
-        await this.vault.setConverter(this.converter.address);
-        await this.vault.setVaultManager(this.vaultManager.address);
-        await this.vault.setTreasuryWallet(treasury);
-        await this.vault.setController(this.controller.address);
-        await this.vaultManager.setVaultStatus(this.vault.address, true);
-        await this.vaultManager.setControllerStatus(this.controller.address, true);
-        await this.vaultManager.setTreasury(treasury);
-        await this.vaultManager.setStakingPool(stakingPool);
-        // await this.vaultManager.setWithdrawalProtectionFee(0);
-        // await this.vaultManager.setStakingPoolShareFee(0);
-        // await this.vaultManager.setTreasuryFee(0);
-        await this.vaultManager.setHarvester(this.harvester.address);
-        await this.harvester.setController(this.controller.address);
-        await this.harvester.addStrategy(this.t3crv.address, this.strategy.address, 0);
-        await this.harvester.setHarvester(deployer, true);
-        await this.controller.setVault(this.t3crv.address, this.vault.address);
-        await this.controller.setConverter(
-            this.t3crv.address,
-            this.dai.address,
-            this.converter.address
-        );
-        await this.controller.addStrategy(this.t3crv.address, this.strategy.address, 0);
-        await this.converter.setStrategy(this.strategy.address, true);
-        await this.strategy.setUnirouter(this.unirouter.address);
-        await this.strategy.approveForSpender(this.weth.address, this.unirouter.address, MAX);
-        await this.strategy.approveForSpender(this.df.address, this.unirouter.address, MAX);
-
-        // Fund addresses
-        await this.yax.mint(this.vault.address, INIT_BALANCE);
-        await this.dai.mint(this.stableSwap3Pool.address, INIT_BALANCE);
-        await this.usdc.mint(this.stableSwap3Pool.address, '1000000000');
-        await this.usdt.mint(this.stableSwap3Pool.address, '1000000000');
-        await this.t3crv.mint(this.stableSwap3Pool.address, INIT_BALANCE);
-        await this.yax.mint(this.unirouter.address, INIT_BALANCE);
-        await this.weth.mint(this.unirouter.address, INIT_BALANCE);
-        await this.dai.mint(this.unirouter.address, INIT_BALANCE);
-        await this.usdc.mint(this.unirouter.address, INIT_BALANCE);
-        await this.usdt.mint(this.unirouter.address, INIT_BALANCE);
-
-        // Strategy-specific setup
-        await this.df.mint(this.unirouter.address, INIT_BALANCE);
-        await this.df.mint(this.dRewards.address, INIT_BALANCE);
-
-        // Bob setup
-        await this.dai.mint(bob, INIT_BALANCE);
-        await this.usdc.mint(bob, '1000000000');
-        await this.usdt.mint(bob, '1000000000');
-        await this.t3crv.mint(bob, INIT_BALANCE);
-
-        await this.dai.approve(this.vault.address, MAX, { from: bob });
-        await this.usdc.approve(this.vault.address, MAX, { from: bob });
-        await this.usdt.approve(this.vault.address, MAX, { from: bob });
-        await this.t3crv.approve(this.vault.address, MAX, { from: bob });
-        await this.vault.approve(this.vault.address, MAX, { from: bob });
+        await afterStrategySetup();
     });
-
-    this.printBalances = async (title) => {
-        console.log(title);
-        console.log(
-            'vault 3CRV:     ',
-            fromWei(await this.t3crv.balanceOf(this.vault.address))
-        );
-        console.log(
-            'vault MVLT:     ',
-            fromWei(await this.vault.balanceOf(this.vault.address))
-        );
-        console.log('vault Supply:   ', fromWei(await this.vault.totalSupply()));
-        console.log('-------------------');
-        console.log(
-            'bob balances:    %s DAI/ %s USDC/ %s USDT/ %s 3CRV/ %s YAX',
-            fromWei(await this.dai.balanceOf(bob)),
-            fromWeiWithDecimals(await this.usdc.balanceOf(bob), 6),
-            fromWeiWithDecimals(await this.usdt.balanceOf(bob), 6),
-            fromWei(await this.t3crv.balanceOf(bob)),
-            fromWei(await this.yax.balanceOf(bob))
-        );
-        console.log('bob staked:     ', fromWei((await this.vault.userInfo(bob)).amount));
-        console.log('-------------------');
-        console.log('stakingPool YAX:', fromWei(await this.yax.balanceOf(stakingPool)));
-        console.log('treasury YAX:   ', fromWei(await this.yax.balanceOf(treasury)));
-        console.log('-------------------');
-    };
 
     beforeEach(async () => {
         if (verbose) {
-            await this.printBalances('\n====== BEFORE ======');
+            await printBalances('\n====== BEFORE ======');
         }
-        assert.equal(0, await this.t3crv.balanceOf(this.controller.address));
-        assert.equal(0, await this.t3crv.balanceOf(this.strategy.address));
-        assert.equal(0, await this.dai.balanceOf(this.strategy.address));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.controller.address));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.strategy.address));
+        assert.equal(0, await globalThis.dai.balanceOf(globalThis.strategy.address));
     });
 
     afterEach(async () => {
         if (verbose) {
-            await this.printBalances('\n====== AFTER ======');
+            await printBalances('\n====== AFTER ======');
         }
-        assert.equal(0, await this.t3crv.balanceOf(this.controller.address));
-        assert.equal(0, await this.t3crv.balanceOf(this.strategy.address));
-        assert.equal(0, await this.dai.balanceOf(this.strategy.address));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.controller.address));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.strategy.address));
+        assert.equal(0, await globalThis.dai.balanceOf(globalThis.strategy.address));
     });
 
     it('should deploy with initial state set', async () => {
         // BaseStrategy
-        assert.equal(this.dai.address, await this.strategy.want());
-        assert.equal(this.weth.address, await this.strategy.weth());
-        assert.equal(this.controller.address, await this.strategy.controller());
-        assert.equal(this.vaultManager.address, await this.strategy.vaultManager());
-        assert.equal(this.unirouter.address, await this.strategy.unirouter());
+        assert.equal(globalThis.dai.address, await globalThis.strategy.want());
+        assert.equal(globalThis.weth.address, await globalThis.strategy.weth());
+        assert.equal(globalThis.controller.address, await globalThis.strategy.controller());
+        assert.equal(
+            globalThis.vaultManager.address,
+            await globalThis.strategy.vaultManager()
+        );
+        assert.equal(globalThis.unirouter.address, await globalThis.strategy.unirouter());
 
         // Implementation
-        assert.equal(this.dDai.address, await this.strategy.dToken());
-        assert.equal(this.dRewards.address, await this.strategy.pool());
-        assert.equal(this.df.address, await this.strategy.DF());
-        assert.equal(this.converter.address, await this.strategy.converter());
+        assert.equal(globalThis.dDai.address, await globalThis.strategy.dToken());
+        assert.equal(globalThis.dRewards.address, await globalThis.strategy.pool());
+        assert.equal(globalThis.df.address, await globalThis.strategy.DF());
+        assert.equal(globalThis.converter.address, await globalThis.strategy.converter());
     });
 
     it('should deposit DAI', async () => {
         const _amount = ether('10');
-        await this.vault.deposit(_amount, this.dai.address, 1, true, { from: bob });
-        assert.equal(String(await this.dai.balanceOf(bob)), ether('990'));
-        assert.isTrue((await this.controller.balanceOf(this.t3crv.address)).gt(ether('9')));
-        assert.isTrue(ether('1').eq(await this.vault.getPricePerFullShare()));
+        await globalThis.vault.deposit(_amount, globalThis.dai.address, 1, true, {
+            from: globalThis.user
+        });
+        assert.equal(String(await globalThis.dai.balanceOf(globalThis.user)), ether('990'));
+        assert.isTrue(
+            (await globalThis.controller.balanceOf(globalThis.t3crv.address)).gt(ether('9'))
+        );
+        assert.isTrue(ether('1').gte(await globalThis.vault.getPricePerFullShare()));
     });
 
     it('should harvest', async () => {
-        assert.isTrue(ether('1').eq(await this.vault.getPricePerFullShare()));
-        assert.equal(0, await this.yax.balanceOf(stakingPool));
-        assert.equal(0, await this.yax.balanceOf(treasury));
-        await this.harvester.harvestNextStrategy(this.t3crv.address);
-        assert.isTrue((await this.vault.getPricePerFullShare()).gt(ether('1')));
-        assert.isTrue((await this.yax.balanceOf(stakingPool)).gte(ether('0.095')));
-        assert.isTrue((await this.yax.balanceOf(treasury)).gte(ether('0.023')));
+        assert.equal(0, await globalThis.yax.balanceOf(globalThis.stakingPool));
+        assert.equal(0, await globalThis.yax.balanceOf(globalThis.treasury));
+        await globalThis.harvester.harvestNextStrategy(globalThis.t3crv.address);
+        assert.isTrue((await globalThis.vault.getPricePerFullShare()).gt(ether('1')));
+        assert.isTrue(
+            (await globalThis.yax.balanceOf(globalThis.stakingPool)).gte(ether('0.095'))
+        );
+        assert.isTrue(
+            (await globalThis.yax.balanceOf(globalThis.treasury)).gte(ether('0.023'))
+        );
     });
 
     it('should withdraw to DAI', async () => {
-        assert.isTrue(ether('10.02').eq((await this.vault.userInfo(bob)).amount));
-        await this.vault.withdraw(ether('5'), this.dai.address, { from: bob });
-        assert.isTrue(ether('5.02').eq((await this.vault.userInfo(bob)).amount));
-        assert.isTrue((await this.dai.balanceOf(bob)).gte(ether('994.99')));
+        assert.isTrue(
+            ether('10.02').eq((await globalThis.vault.userInfo(globalThis.user)).amount)
+        );
+        await globalThis.vault.withdraw(ether('5'), globalThis.dai.address, {
+            from: globalThis.user
+        });
+        assert.isTrue(
+            ether('5.02').eq((await globalThis.vault.userInfo(globalThis.user)).amount)
+        );
+        assert.isTrue((await globalThis.dai.balanceOf(globalThis.user)).gte(ether('994.99')));
     });
 
     it('should withdrawAll to 3CRV', async () => {
-        await this.vault.withdrawAll(this.t3crv.address, { from: bob });
-        assert.equal(0, await this.vault.balanceOf(bob));
-        assert.equal(0, await this.t3crv.balanceOf(this.controller.address));
-        assert.equal(0, await this.t3crv.balanceOf(this.strategy.address));
-        assert.equal(0, await this.vault.totalSupply());
-        assert.isTrue((await this.t3crv.balanceOf(bob)).gte(ether('1005')));
+        await globalThis.vault.withdrawAll(globalThis.t3crv.address, {
+            from: globalThis.user
+        });
+        assert.equal(0, await globalThis.vault.balanceOf(globalThis.user));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.controller.address));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.strategy.address));
+        assert.equal(0, await globalThis.vault.totalSupply());
+        assert.isTrue((await globalThis.t3crv.balanceOf(globalThis.user)).gte(ether('1005')));
     });
 
     it('should deposit USDT', async () => {
         const _amount = '10000000';
-        assert.equal(0, await this.strategy.balanceOfPool());
-        assert.equal(0, await this.controller.balanceOf(this.t3crv.address));
-        await this.vault.deposit(_amount, this.usdt.address, 1, true, { from: bob });
-        assert.isTrue((await this.strategy.balanceOfPool()).gt(ether('9')));
-        assert.isTrue((await this.controller.balanceOf(this.t3crv.address)).gt(ether('9')));
-        assert.isTrue((await this.vault.getPricePerFullShare()).gte(ether('1')));
+        assert.equal(0, await globalThis.strategy.balanceOfPool());
+        assert.equal(0, await globalThis.controller.balanceOf(globalThis.t3crv.address));
+        await globalThis.vault.deposit(_amount, globalThis.usdt.address, 1, true, {
+            from: globalThis.user
+        });
+        assert.isTrue((await globalThis.strategy.balanceOfPool()).gt(ether('9')));
+        assert.isTrue(
+            (await globalThis.controller.balanceOf(globalThis.t3crv.address)).gt(ether('9'))
+        );
+        assert.isTrue((await globalThis.vault.getPricePerFullShare()).gte(ether('1')));
     });
 
     it('should withdrawAll by controller', async () => {
-        assert.equal(0, await this.t3crv.balanceOf(this.controller.address));
-        assert.isTrue((await this.t3crv.balanceOf(this.vault.address)).lt(ether('1')));
-        await this.controller.withdrawAll(this.strategy.address);
-        assert.isTrue((await this.t3crv.balanceOf(this.vault.address)).gte(ether('9.99')));
+        assert.equal(0, await globalThis.t3crv.balanceOf(globalThis.controller.address));
+        assert.isTrue(
+            (await globalThis.t3crv.balanceOf(globalThis.vault.address)).lt(ether('1'))
+        );
+        await globalThis.controller.withdrawAll(globalThis.strategy.address);
+        assert.isTrue(
+            (await globalThis.t3crv.balanceOf(globalThis.vault.address)).gte(ether('9.99'))
+        );
     });
 });
