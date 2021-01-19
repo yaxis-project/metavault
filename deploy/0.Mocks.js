5b835478d673c164dd9b5e11494c20aeb5515dc8
Aaron Zinghini <aaron.zinghini@seeingmachines.com>
2021-01-18 19:01:32 -0700

    Initial commit of idle.finance strategy

--- deploy/0.Mocks.js
+++ deploy/0.Mocks.js
@@ -10,6 +10,12 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
             contract: 'MockERC20',
             args: ['yAxis', 'YAX', 18]
         });
+        await deploy('COMP', {
+            from: deployer,
+            log: true,
+            contract: 'MockERC20',
+            args: ['Compound', 'COMP', 18]
+        });
         await deploy('DAI', {
             from: deployer,
             log: true,
---
+++ deploy/6.5.StrategyIdle.js
@@ -0,0 +1,99 @@
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+    const { deploy, execute } = deployments;
+    let {
+        DAI,
+        IDLE,
+        COMP,
+        WETH,
+        idleDAI,
+        deployer,
+        unirouter
+    } = await getNamedAccounts();
+    const chainId = await getChainId();
+    const controller = await deployments.get('StrategyControllerV2');
+    const vaultManager = await deployments.get('yAxisMetaVaultManager');
+    const Converter = await deployments.get('StableSwap3PoolConverter');
+
+    if (chainId != '1') {
+        const dai = await deployments.get('DAI');
+        DAI = dai.address;
+        const comp = await deployments.get('COMP');
+        COMP = comp.address;
+        const weth = await deployments.get('WETH');
+        WETH = weth.address;
+        await deploy('IDLE', {
+            from: deployer,
+            contract: 'MockERC20',
+            log: true,
+            args: ['IDLE Token', 'IDLE', 18]
+        });
+        IDLE = await deployments.get('IDLE');
+        IDLE = IDLE.address;
+        const deployedIdleDAI = await deploy('idleDAI', {
+            from: deployer,
+            contract: 'MockIdleToken',
+            log: true,
+            args: ['Idle Token DAI', 'idleDAIYield', dai.address, IDLE, COMP]
+        });
+        const idleDai = await deployments.get('idleDAI');
+        idleDAI = idleDai.address;
+        const router = await deployments.get('MockUniswapRouter');
+        unirouter = router.address;
+        if (deployedIdleDAI.newlyDeployed) {
+            await execute(
+                'IDLE',
+                { from: deployer },
+                'mint',
+                router.address,
+                ethers.utils.parseEther('1000')
+            );
+            await execute(
+                'COMP',
+                { from: deployer },
+                'mint',
+                router.address,
+                ethers.utils.parseEther('1000')
+            );
+            await execute(
+                'IDLE',
+                { from: deployer },
+                'mint',
+                idleDai.address,
+                ethers.utils.parseEther('1000')
+            );
+            await execute(
+                'COMP',
+                { from: deployer },
+                'mint',
+                idleDai.address,
+                ethers.utils.parseEther('1000')
+            );
+        }
+    }
+
+    const deployedStrategy = await deploy('StrategyIdle', {
+        from: deployer,
+        log: true,
+        args: [
+            DAI,
+            idleDAI,
+            IDLE,
+            COMP,
+            Converter.address,
+            controller.address,
+            vaultManager.address,
+            WETH,
+            unirouter
+        ]
+    });
+    const Strategy = await deployments.get('StrategyIdle');
+    if (deployedStrategy.newlyDeployed) {
+        await execute(
+            'StableSwap3PoolConverter',
+            { from: deployer },
+            'setStrategy',
+            Strategy.address,
+            true
+        );
+    }
+};
