03d5a2f3f24a479a8d24c3dd3219183ca0ebaed9
transferAndCall <transferAndCall@yaxis.io>
2021-06-17 13:17:34 -0500

    Update YaxisVotePower to use new token and Rewards

--- contracts/governance/YaxisVotePower.sol
+++ contracts/governance/YaxisVotePower.sol
@@ -3,63 +3,42 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
-import "./interfaces/IMasterChef.sol";
+import "./interfaces/IRewards.sol";
 import "./interfaces/IUniswapV2Pair.sol";
 import "./interfaces/IVoteProxy.sol";
-import "./interfaces/IYaxisBar.sol";
 
 contract YaxisVotePower is IVoteProxy {
     using SafeMath for uint256;
 
-    uint256 public constant PID = 6;
     // solhint-disable-next-line const-name-snakecase
     uint8 public constant override decimals = uint8(18);
 
-    // ETH/YAX token
-    IUniswapV2Pair public immutable yaxEthUniswapV2Pair;
-
-    // YAX token
-    IERC20 public immutable yax;
-
-    // YaxisChef contract
-    IMasterChef public immutable chef;
-
-    // sYAX token
-    IYaxisBar public immutable yaxisBar;
+    IUniswapV2Pair public immutable yaxisEthUniswapV2Pair;
+    IERC20 public immutable yaxis;
+    IRewards public immutable rewardsYaxis;
+    IRewards public immutable rewardsYaxisEth;
 
     constructor(
-        address _yax,
-        address _yaxisChef,
-        address _yaxisBar,
-        address _yaxEthUniswapV2Pair
+        address _yaxis,
+        address _rewardsYaxis,
+        address _rewardsYaxisEth,
+        address _yaxisEthUniswapV2Pair
     )
         public
     {
-        yax = IERC20(_yax);
-        chef = IMasterChef(_yaxisChef);
-        yaxisBar = IYaxisBar(_yaxisBar);
-        yaxEthUniswapV2Pair = IUniswapV2Pair(_yaxEthUniswapV2Pair);
+        yaxis = IERC20(_yaxis);
+        rewardsYaxis = IRewards(_rewardsYaxis);
+        rewardsYaxisEth = IRewards(_rewardsYaxisEth);
+        yaxisEthUniswapV2Pair = IUniswapV2Pair(_yaxisEthUniswapV2Pair);
     }
 
     function totalSupply()
         external
         view
         override
-        returns (uint256 _supply)
+        returns (uint256)
     {
-        (uint256 _yaxReserves,,) = yaxEthUniswapV2Pair.getReserves();
-        _supply = yaxEthUniswapV2Pair.totalSupply();
-        _supply = _supply == 0
-            ? 1e18
-            : _supply;
-        uint256 _lpStakingYax = _yaxReserves
-            .mul(yaxEthUniswapV2Pair.balanceOf(address(chef)))
-            .div(_supply);
-        _supply = sqrt(
-            yax.totalSupply()
-                .add(_lpStakingYax)
-                .add(yaxisBar.availableBalance())
-        );
+        return sqrt(yaxis.totalSupply());
     }
 
     function balanceOf(
@@ -70,27 +49,22 @@ contract YaxisVotePower is IVoteProxy {
         override
         returns (uint256 _balance)
     {
-        (uint256 _stakeAmount,,) = chef.userInfo(PID, _voter);
-        (uint256 _yaxReserves,,) = yaxEthUniswapV2Pair.getReserves();
-        uint256 _supply = yaxEthUniswapV2Pair.totalSupply();
+        uint256 _stakeAmount = rewardsYaxisEth.balanceOf(_voter);
+        (uint256 _yaxReserves,,) = yaxisEthUniswapV2Pair.getReserves();
+        uint256 _supply = yaxisEthUniswapV2Pair.totalSupply();
         _supply = _supply == 0
             ? 1e18
             : _supply;
         uint256 _lpStakingYax = _yaxReserves
             .mul(_stakeAmount)
             .div(_supply)
-            .add(chef.pendingYaxis(PID, _voter));
-        _supply = yaxisBar.totalSupply();
-        _supply = _supply == 0
-            ? 1e18
-            : _supply;
-        uint256 _syaxAmount = yaxisBar.balanceOf(_voter)
-            .mul(yaxisBar.availableBalance())
-            .div(_supply);
+            .add(rewardsYaxisEth.earned(_voter));
+        uint256 _rewardsYaxisAmount = rewardsYaxis.balanceOf(_voter)
+            .add(rewardsYaxis.earned(_voter));
         _balance = sqrt(
-            yax.balanceOf(_voter)
+            yaxis.balanceOf(_voter)
                 .add(_lpStakingYax)
-                .add(_syaxAmount)
+                .add(_rewardsYaxisAmount)
         );
     }
 
