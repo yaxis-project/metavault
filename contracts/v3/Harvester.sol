532c6de1176630cd84d566542e25bcf2f8c837f3
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2021-10-17 20:29:03 +0300

    getEstimates() and harvest() have big changes
    This should now accurately calculate every swap and addLiquidity done while harvest with allowed slippage, so frontrunning harvests should no longer be a problem.

--- contracts/v3/Harvester.sol
+++ contracts/v3/Harvester.sol
@@ -11,6 +11,10 @@ import "./interfaces/ILegacyController.sol";
 import "./interfaces/IManager.sol";
 import "./interfaces/IStrategy.sol";
 import "./interfaces/ISwap.sol";
+import "./interfaces/IStableSwapPool.sol";
+import "./interfaces/ExtendedIERC20.sol";
+import "./interfaces/ICVXMinter.sol";
+import "./interfaces/IConvexVault.sol";
 
 /**
  * @title Harvester
@@ -167,12 +171,13 @@ contract Harvester is IHarvester {
 
     function earn(
         address _strategy,
-        address _vault
+        IVault _vault,
+        address _token
     )
         external
         onlyHarvester
     {
-        IVault(_vault).earn(_strategy);
+        _vault.earn(_token, _strategy);
     }
 
     /**
@@ -184,13 +189,17 @@ contract Harvester is IHarvester {
     function harvest(
         IController _controller,
         address _strategy,
-        uint256 _estimatedWETH,
-        uint256 _estimatedYAXIS
+        uint256 _estimatedCRVWETH,
+        uint256 _estimatedCVXWETH,
+        uint256 _estimatedYAXIS,
+        uint256[] memory _estimatedExtraWETH,
+        uint256 _estimatedToken,
+        uint256 _estimatedWant
     )
         public
         onlyHarvester
     {
-        _controller.harvestStrategy(_strategy, _estimatedWETH, _estimatedYAXIS);
+        _controller.harvestStrategy(_strategy, _estimatedCRVWETH, _estimatedCVXWETH, _estimatedYAXIS, _estimatedExtraWETH, _estimatedToken, _estimatedWant);
         emit Harvest(address(_controller), _strategy);
     }
 
@@ -201,14 +210,18 @@ contract Harvester is IHarvester {
      */
     function harvestNextStrategy(
         address _vault,
-        uint256 _estimatedWETH,
-        uint256 _estimatedYAXIS
+        uint256 _estimatedCRVWETH,
+        uint256 _estimatedCVXWETH,
+        uint256 _estimatedYAXIS,
+        uint256[] memory _estimatedExtraWETH,
+        uint256 _estimatedToken,
+        uint256 _estimatedWant
     )
         external
     {
         require(canHarvest(_vault), "!canHarvest");
         address strategy = strategies[_vault].addresses[0];
-        harvest(controller, strategy, _estimatedWETH, _estimatedYAXIS);
+        harvest(controller, strategy, _estimatedCRVWETH, _estimatedCVXWETH, _estimatedYAXIS, _estimatedExtraWETH, _estimatedToken, _estimatedWant);
         uint256 k = strategies[_vault].addresses.length;
         if (k > 1) {
             address[] memory _strategies = new address[](k);
@@ -224,15 +237,17 @@ contract Harvester is IHarvester {
 
     /**
      * @notice Earns tokens in the LegacyController to the v3 vault
+     * @param _token The address of the token
      * @param _expected The expected amount to deposit after conversion
      */
     function legacyEarn(
+        address _token,
         uint256 _expected
     )
         external
         onlyHarvester
     {
-        legacyController.legacyDeposit(_expected);
+        legacyController.legacyDeposit(_token, _expected);
     }
 
     /**
@@ -270,7 +285,6 @@ contract Harvester is IHarvester {
     {
         Strategy storage strategy = strategies[_vault];
         // only can harvest if there are strategies, and when sufficient time has elapsed
-        // solhint-disable-next-line not-rely-on-time
         return (strategy.addresses.length > 0 && strategy.lastCalled <= block.timestamp.sub(strategy.timeout));
     }
 
@@ -283,28 +297,129 @@ contract Harvester is IHarvester {
     )
         public
         view
-        returns (uint256 _estimatedWETH, uint256 _estimatedYAXIS)
+        returns (uint256 _estimatedCRVWETH, uint256 _estimatedCVXWETH, uint256 _estimatedYAXIS, uint256[] memory _estimatedExtraWETH, uint256 _estimatedToken, uint256 _estimatedWant)
     {
-        ISwap _router = IStrategy(_strategy).router();
-        address[] memory _path;
-        _path[0] = IStrategy(_strategy).want();
-        _path[1] = IStrategy(_strategy).weth();
-        uint256[] memory _amounts = _router.getAmountsOut(
-            IStrategy(_strategy).balanceOfPool(),
-            _path
-        );
-        _estimatedWETH = _amounts[1];
+        IStrategy strategy = IStrategy(_strategy);
+        ISwap _router = strategy.router();
         uint256 _slippage = slippage;
-        if (_slippage > 0) {
-            _estimatedWETH = _estimatedWETH.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+        address[] memory _path;
+        uint256[] memory _amounts;
+
+        // Estimates for CRV -> WETH
+        if (strategy.weth() != address(0) && strategy.crv() != address(0)) {
+            _path[0] = strategy.crv();
+            _path[1] = strategy.weth();
+            _amounts = _router.getAmountsOut(
+                strategy.crvRewards().earned(address(this)),
+                _path
+            );
+            _estimatedCRVWETH = _amounts[1];
+            
+            if (_slippage > 0) {
+                _estimatedCRVWETH = _estimatedCRVWETH - _estimatedCRVWETH.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+            }
         }
-        _path[0] = manager.yaxis();
-        uint256 _fee = _estimatedWETH.mul(manager.treasuryFee()).div(ONE_HUNDRED_PERCENT);
-        _amounts = _router.getAmountsOut(_fee, _path);
+
+        // Estimates for CVX -> WETH
+        if (strategy.weth() != address(0) && strategy.cvx() != address(0)) {
+            _path[0] = strategy.cvx();
+            _path[1] = strategy.weth();
+            ICVXMinter minter = ICVXMinter(strategy.cvx());
+            _amounts = _router.getAmountsOut(
+                // Calculating CVX minted
+                (strategy.crvRewards().earned(address(this))).mul(minter.totalCliffs().sub(minter.maxSupply().div(minter.reductionPerCliff()))).div(minter.totalCliffs()),
+                _path
+            );
+            _estimatedCVXWETH = _amounts[1];
+            
+            if (_slippage > 0) {
+                _estimatedCVXWETH = _estimatedCVXWETH - _estimatedCVXWETH.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+            }
+        }
+        
+        // Estimates for extra rewards -> WETH
+        uint256 _extraRewardsLength = strategy.crvRewards().extraRewardsLength();
+        if (_extraRewardsLength > 0) {
+            for (uint256 i = 0; i < _extraRewardsLength; i++) {
+                _path[0] = IConvexRewards(strategy.crvRewards().extraRewards(i)).rewardToken();
+                _path[1] = strategy.weth();
+                _amounts = _router.getAmountsOut(
+                    IConvexRewards(strategy.crvRewards().extraRewards(i)).earned(address(this)),
+                    _path
+                );
+                _estimatedExtraWETH[i] = _amounts[1];
+                if (_slippage > 0) {
+                    _estimatedExtraWETH[i] = _estimatedExtraWETH[i] - _estimatedExtraWETH[i].mul(_slippage).div(ONE_HUNDRED_PERCENT);
+                }
+            }
+        }
+        
+        // Estimates WETH -> YAXIS
+        _path[0] = strategy.weth();
+        _path[1] = manager.yaxis();
+        uint256 _beforeFee = _estimatedCRVWETH + _estimatedCVXWETH;
+        if (_extraRewardsLength > 0) {
+            for (uint256 i = 0; i < _extraRewardsLength; i++) {
+                _beforeFee += _estimatedExtraWETH[i];
+            }
+        }
+        uint256 _fee = _beforeFee.mul(manager.treasuryFee()).div(ONE_HUNDRED_PERCENT);
+        _amounts = ISwap(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D).getAmountsOut(_fee, _path); // Set to UniswapV2 to calculate output for YAXIS
         _estimatedYAXIS = _amounts[1];
         if (_slippage > 0) {
-            _estimatedYAXIS = _estimatedYAXIS.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+            _estimatedYAXIS = _estimatedYAXIS - _estimatedYAXIS.mul(_slippage).div(ONE_HUNDRED_PERCENT);
         }
+
+        // Estimates for WETH -> 3CRV || Estimates for WETH -> Pooled token -> Want
+        (address strategyPremium,) = strategy.getMostPremium();
+        if (strategyPremium == strategy.crv3()) {
+            IStableSwapPool strategyStablePool = IStableSwapPool(strategy.stableSwapPool());
+            address premiumStable = getPremiumStable(strategy);
+            _path[0] = strategy.weth();
+            _path[1] = premiumStable;
+            _amounts = _router.getAmountsOut(
+                _beforeFee - _fee,
+                _path
+            );
+            if (premiumStable == 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48 || premiumStable == 0xdAC17F958D2ee523a2206206994597C13D831ec7) {
+                _amounts[1] = _amounts[1].mul(10**18).div(10**(ExtendedIERC20(premiumStable).decimals()));
+            }
+            _estimatedToken = _amounts[1];
+            _estimatedWant = _amounts[1].div(strategyStablePool.get_virtual_price());
+            if (_slippage > 0) {
+                _estimatedToken = _amounts[1] - _amounts[1].mul(_slippage).div(ONE_HUNDRED_PERCENT);
+                _estimatedWant = _estimatedWant - _estimatedWant.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+            }
+        }
+        else {
+            _path[0] = strategy.weth();
+            _path[1] = strategyPremium;
+            _amounts = _router.getAmountsOut(
+                _beforeFee - _fee,
+                _path
+            );
+            _estimatedToken = _amounts[1];
+            _estimatedWant = _amounts[1].div(IStableSwapPool(strategy.want()).get_virtual_price());
+            if (_slippage > 0) {
+                _estimatedToken = _estimatedToken - _estimatedToken.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+                _estimatedWant = _estimatedWant - _estimatedWant.mul(_slippage).div(ONE_HUNDRED_PERCENT);
+            }            
+        }
+    }
+
+    function getPremiumStable(IStrategy strategy) private view returns (address) {
+        IStableSwapPool stableSwapPool = IStableSwapPool(strategy.crv3());
+        uint daiBalance = stableSwapPool.balances(0);
+        uint usdcBalance = stableSwapPool.balances(1).mul(10**18).div(10**(ExtendedIERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48).decimals())); // USDC address
+        uint usdtBalance = stableSwapPool.balances(2).mul(10**12);
+        address premiumStable = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // DAI
+        if (usdcBalance <= daiBalance && usdcBalance <= usdtBalance) {
+            premiumStable = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // USDC
+        }
+        if (usdtBalance <= daiBalance && usdtBalance <= usdcBalance) {
+            premiumStable = 0xdAC17F958D2ee523a2206206994597C13D831ec7; // USDT
+        }
+        return premiumStable; 
     }
 
     /**
