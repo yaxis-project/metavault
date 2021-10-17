faf257ba840f4e3b484f69802f4b4ae7e574a920
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2021-10-17 20:36:57 +0300

    Added new useful tokens to the interface
    They have to be overridden in strategies even they are not used, it standardizes token variable names

--- contracts/v3/interfaces/IStrategy.sol
+++ contracts/v3/interfaces/IStrategy.sol
@@ -4,17 +4,25 @@ pragma solidity 0.6.12;
 
 import "./IManager.sol";
 import "./ISwap.sol";
+import "./IStableSwapPool.sol";
+import "./IConvexVault.sol";
 
 interface IStrategy {
     function balanceOf() external view returns (uint256);
     function balanceOfPool() external view returns (uint256);
     function balanceOfWant() external view returns (uint256);
+    function crv3() external view returns (address);
+    function crv() external view returns (address);
+    function crvRewards() external view returns (IConvexRewards);
+    function cvx() external view returns (address);
     function deposit() external;
-    function harvest(uint256, uint256) external;
+    function getMostPremium() external view returns (address, uint256);
+    function harvest(uint256, uint256, uint256, uint256[] memory, uint256, uint256) external;
     function manager() external view returns (IManager);
     function name() external view returns (string memory);
     function router() external view returns (ISwap);
     function skim() external;
+    function stableSwapPool() external view returns (IStableSwapPool);
     function want() external view returns (address);
     function weth() external view returns (address);
     function withdraw(address) external;
