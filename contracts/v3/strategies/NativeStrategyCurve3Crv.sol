94760447eba044dfc5c996300be07f941cfedf5c
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2021-10-17 20:30:48 +0300

    Made it compatible with getEstimates().

--- contracts/v3/strategies/NativeStrategyCurve3Crv.sol
+++ contracts/v3/strategies/NativeStrategyCurve3Crv.sol
@@ -11,15 +11,20 @@ import "./BaseStrategy.sol";
 
 contract NativeStrategyCurve3Crv is BaseStrategy {
     // used for Crv -> weth -> [dai/usdc/usdt] -> 3crv route
-    address public immutable crv;
+    address public immutable override crv;
 
     // for add_liquidity via curve.fi to get back 3CRV (use getMostPremium() for the best stable coin used in the route)
     address public immutable dai;
     address public immutable usdc;
     address public immutable usdt;
+    
+    // Unused tokens in strategy
+    address public override crv3;
+    address public override cvx;
+    IConvexRewards public override crvRewards;
 
     Mintr public immutable crvMintr;
-    IStableSwap3Pool public immutable stableSwap3Pool;
+    IStableSwapPool public immutable override stableSwapPool;
     Gauge public immutable gauge; // 3Crv Gauge
 
     constructor(
@@ -32,7 +37,7 @@ contract NativeStrategyCurve3Crv is BaseStrategy {
         address _usdt,
         Gauge _gauge,
         Mintr _crvMintr,
-        IStableSwap3Pool _stableSwap3Pool,
+        IStableSwapPool _stableSwapPool,
         address _controller,
         address _manager,
         address _router
@@ -44,15 +49,15 @@ contract NativeStrategyCurve3Crv is BaseStrategy {
         dai = _dai;
         usdc = _usdc;
         usdt = _usdt;
-        stableSwap3Pool = _stableSwap3Pool;
+        stableSwapPool = _stableSwapPool;
         gauge = _gauge;
         crvMintr = _crvMintr;
         IERC20(_want).safeApprove(address(_gauge), type(uint256).max);
         IERC20(_crv).safeApprove(address(_router), type(uint256).max);
-        IERC20(_dai).safeApprove(address(_stableSwap3Pool), type(uint256).max);
-        IERC20(_usdc).safeApprove(address(_stableSwap3Pool), type(uint256).max);
-        IERC20(_usdt).safeApprove(address(_stableSwap3Pool), type(uint256).max);
-        IERC20(_want).safeApprove(address(_stableSwap3Pool), type(uint256).max);
+        IERC20(_dai).safeApprove(address(_stableSwapPool), type(uint256).max);
+        IERC20(_usdc).safeApprove(address(_stableSwapPool), type(uint256).max);
+        IERC20(_usdt).safeApprove(address(_stableSwapPool), type(uint256).max);
+        IERC20(_want).safeApprove(address(_stableSwapPool), type(uint256).max);
     }
 
     function _deposit()
@@ -72,25 +77,26 @@ contract NativeStrategyCurve3Crv is BaseStrategy {
         crvMintr.mint(address(gauge));
     }
 
-    function _addLiquidity()
+    function _addLiquidity(uint256 _estimatedWant)
         internal
     {
         uint256[3] memory amounts;
         amounts[0] = IERC20(dai).balanceOf(address(this));
         amounts[1] = IERC20(usdc).balanceOf(address(this));
         amounts[2] = IERC20(usdt).balanceOf(address(this));
-        stableSwap3Pool.add_liquidity(amounts, 1);
+        stableSwapPool.add_liquidity(amounts, _estimatedWant);
     }
 
     function getMostPremium()
         public
         view
+        override
         returns (address, uint256)
     {
-        uint daiBalance = stableSwap3Pool.balances(0);
+        uint daiBalance = stableSwapPool.balances(0);
         // USDC - Supports a change up to the 18 decimal standard
-        uint usdcBalance = stableSwap3Pool.balances(1).mul(10**18).div(10**(ExtendedIERC20(usdc).decimals()));
-        uint usdtBalance = stableSwap3Pool.balances(2).mul(10**12);
+        uint usdcBalance = stableSwapPool.balances(1).mul(10**18).div(10**(ExtendedIERC20(usdc).decimals()));
+        uint usdtBalance = stableSwapPool.balances(2).mul(10**12);
 
         if (daiBalance <= usdcBalance && daiBalance <= usdtBalance) {
             return (dai, 0);
@@ -108,19 +114,23 @@ contract NativeStrategyCurve3Crv is BaseStrategy {
     }
 
     function _harvest(
-        uint256 _estimatedWETH,
-        uint256 _estimatedYAXIS
+        uint256 _estimatedCRVWETH,
+        uint256 _estimatedCVXWETH,
+        uint256 _estimatedYAXIS,
+        uint256[] memory _estimatedExtraWETH,
+        uint256 _estimatedToken,
+        uint256 _estimatedWant
     )
         internal
         override
     {
         _claimReward();
-        uint256 _remainingWeth = _payHarvestFees(crv, _estimatedWETH, _estimatedYAXIS);
+        uint256 _remainingWeth = _payHarvestFees(crv, _estimatedCRVWETH, _estimatedYAXIS);
 
         if (_remainingWeth > 0) {
             (address _stableCoin,) = getMostPremium(); // stablecoin we want to convert to
-            _swapTokens(weth, _stableCoin, _remainingWeth, 1);
-            _addLiquidity();
+            _swapTokens(weth, _stableCoin, _remainingWeth, _estimatedToken);
+            _addLiquidity(_estimatedWant);
 
             _deposit();
         }
