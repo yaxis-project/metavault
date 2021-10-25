977f9bf9340bbfbd64c330a367cad9d4ecfab998
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2021-10-25 13:35:55 +0300

    Convex3CRV test: Router array + estimates array

--- test/v3/ConvexStrategy3Crv.test.js
+++ test/v3/ConvexStrategy3Crv.test.js
@@ -69,7 +69,7 @@ describe('ConvexStrategy3Crv', () => {
                 stableSwap3Pool.address,
                 controller.address,
                 manager.address,
-                unirouter.address
+                [unirouter.address, unirouter.address]
             ]
         });
         convexStrategy = await ethers.getContractAt('ConvexStrategy', ConvexStrategy.address);
@@ -118,13 +118,13 @@ describe('ConvexStrategy3Crv', () => {
     describe('setRouter', () => {
         it('should revert if called by an address other than governance', async () => {
             await expect(
-                convexStrategy.connect(user).setRouter(ethers.constants.AddressZero)
+                convexStrategy.connect(user).setRouter([ethers.constants.AddressZero], [dai.address])
             ).to.be.revertedWith('!governance');
         });
 
         it('should set router when called by governance', async () => {
             expect(await convexStrategy.router()).to.equal(unirouter.address);
-            await convexStrategy.connect(treasury).setRouter(ethers.constants.AddressZero);
+            await convexStrategy.connect(treasury).setRouter([ethers.constants.AddressZero], [dai.address]);
             expect(await convexStrategy.router()).to.equal(ethers.constants.AddressZero);
         });
     });
@@ -137,7 +137,7 @@ describe('ConvexStrategy3Crv', () => {
 
     describe('deposit', () => {
         it('should revert if called by an address other than controller', async () => {
-            await expect(convexStrategy.harvest(0, 0)).to.be.revertedWith('!controller');
+            await expect(convexStrategy.harvest([0, 0, 0, 0, 0, 0, 0, 0])).to.be.revertedWith('!controller');
         });
     });
 
