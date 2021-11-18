2fe4cf831beee6d121c5f378756ffef732e2e59a
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2021-11-18 22:15:16 +0200

    Added CVX totalSupply

--- contracts/v3/interfaces/ICVXMinter.sol
+++ contracts/v3/interfaces/ICVXMinter.sol
@@ -4,6 +4,7 @@ pragma solidity 0.6.12;
 
 interface ICVXMinter {
     function maxSupply() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
     function totalCliffs() external view returns (uint256);
     function reductionPerCliff() external view returns (uint256);
 }
