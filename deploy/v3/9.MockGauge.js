dc01b41d19e6173bb73375fc2790634e1e47d86c
Haz <email@e.com>
2022-02-03 17:37:21 +0200

    Mock Gauge Deployment

---
+++ deploy/v3/9.MockGauge.js
@@ -0,0 +1,27 @@
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, execute } = deployments;
+    let { deployer } = await getNamedAccounts();
+
+    const YAXIS = await deployments.get('YaxisToken');
+    const GaugeProxy = await deployments.get('GaugeProxy');
+    const MinterWrapper = await deployments.get('Minter');
+
+    const Gauge = await deploy('MockGauge', {
+        contract: 'LiquidityGaugeV2',
+        from: deployer,
+        log: true,
+        args: [YAXIS.address, MinterWrapper.address, GaugeProxy.address]
+    });
+
+    await execute(
+        'GaugeController',
+        { from: deployer, log: true },
+        'add_gauge(address,int128,uint256)',
+        Gauge.address,
+        0,
+        //Weight to be calulated to match the exact difference between points_total and actual sum of weights
+        ethers.utils.parseEther('1')
+    );
+};
+
+module.exports.tags = ['v3', 'gauges'];
