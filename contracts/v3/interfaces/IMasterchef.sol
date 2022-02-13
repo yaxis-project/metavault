0674106a8bbaca61158842ed4f0ed07140a8d0b4
GainsGoblin <83374187+GainsGoblin@users.noreply.github.com>
2022-02-13 23:47:45 +0100

    Bonus rewards interface

--- contracts/v3/interfaces/IMasterchef.sol
+++ contracts/v3/interfaces/IMasterchef.sol
@@ -8,7 +8,19 @@ interface IMasterchef {
         uint256 amount;
         uint256 rewardDebt;
     }
+    struct PoolInfo {
+        address lpToken;
+        uint256 accJoePerShare;
+        uint256 lastRewardTimestamp;
+        uint256 allocPoint;
+        address rewarder;
+    }
     function userInfo(uint256 pid, address user) external view returns (UserInfo calldata);
     function deposit(uint256 pid, uint256 amount) external;
     function withdraw(uint256 pid, uint256 amount) external;
+    function poolInfo(uint256 pid) external view returns (PoolInfo calldata);
+}
+
+interface IBonusRewards {
+    function rewardToken() external view returns (address);
 }
